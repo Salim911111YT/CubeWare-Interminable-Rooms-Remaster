bded32430ad59bff3479a45da1acf68f3942400f
Salim911111YT <sass.stoy@gmail.com>
2026-09-07 18:00:45 +0200

    Rename Entity ESP to Entity Notifier and update logic

--- Main.lua
+++ Main.lua
@@ -13,7 +13,7 @@ else
 		bad_executor = true
 		Alurt.CreateNode({
 			Name = "CubeWare",
-			Content = "Due to your executor NOT supporting some function (cloneref, firetouchtransmitter, etc...), you won't have access to much modules."
+			Content = "Due to your executor NOT supporting some function (attempt to 'clonerf'), you won't have access to much modules."
 			Length = 20,
 			Image = "rbxassetid://0",
 			BarColor = Color3.fromRGB(75, 75, 75)
@@ -22,7 +22,10 @@ else
 end
 
 local connections = {
-	["EntityESP"] = nil
+	["EntityESP"] = {
+		["1"] = nil,
+		["2"] = nil
+	}
 }
 
 -- Create A Window (Put The Tab Name Behind The Section Name)
@@ -41,13 +44,13 @@ local success, error_ = pcall(function()
 	end)
 end)
 
-local world = Window:MakeTab("World");
+local world = Window:MakeTab("Main");
 
 world:CreateToggle({
-    Name = "Entity ESP",
+    Name = "Entity Notifier",
     Callback = function(value)
 		if value then
-			connections["EntityESP"] = workspace["ENTITIES"].ChildAdded:Connect(function(child)
+			connections["EntityESP"]["1"] = workspace["Entities"].ChildAdded:Connect(function(child)
 				Alurt.CreateNode({
 					Title = "Entity ESP",
 					Content = tostring(child.Name).." has spawned!",
@@ -56,19 +59,62 @@ world:CreateToggle({
 					BarColor = Color3.fromRGB(75, 75, 75)
 				})
 			end)
+
+			connections["EntityESP"]["2"] = workspace["Entities"].ChildRemoved:Connect(function(child)
+				Alurt.CreateNode({
+					Title = "Entity ESP",
+					Content = tostring(child.Name).." has despawned!",
+					Length = 5,
+					Image = "rbxassetid://0",
+					BarColor = Color3.fromRGB(75, 75, 75)
+				})
+			end)
 		else
-			if connections["EntityESP"] ~= nil then
-				connections["EntityESP"]:Disconnect()
-				connections["EntitYESP"] = nil
+			for _, v in pairs(connections["EntityESP"]) do
+				if v then
+					v:Disconnect()
+					v = nil
+				end
 			end
 		end
     end
 })
+
+if hookmetamethod then
+	world:AddButton({
+		Name = "Client Anti-Kick",
+		Callback = function()
+			if hookmetamethod then
+				hookmetamethod(game, "__namecall", function(self, ...)
+    				local methode = getnamecallmethod()
+    				local args = ...
+
+    				if string.lower(methode) == "kick" then
+        				pcall(function()
+							Alurt.CreateNode({
+								Title = "Anti Kick",
+								Content = "Dodged Client Sided Kick with message: "..tostring(unpack(args)),
+								Audio = "rbxassetid://0",
+								Length = 10,
+								Image = "rbxassetid://0",
+								BarColor = Color3.fromRGB(75, 75, 75)
+							})
+						end)
+						
+       					return
+   					end
+				end)
+			else
+				warn("Doesn't support 'hookmetamethod'")
+			end
+		end,
+	})
+end
     
 task.spawn(function()
 	pcall(function()
 		local notif1 = Alurt.CreateNode({
-   			 Title = "Cubeware",
+   			Title = "Cubeware",
    		 	Content = "Cubeware has been loaded!",
    		 	Audio = "rbxassetid://118797914122839",
     		Length = 4,
